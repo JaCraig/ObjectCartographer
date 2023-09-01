fcb9fd6cab5b689902a64cd89d371346de5215e0
JaCraig <craig.jamesa@gmail.com>
2023-09-01 11:30:12 -0400

    chore: Adding test

--- ObjectCartographer.Tests/ExpressionBuilder/Converters/DefaultConverterTests.cs
+++ ObjectCartographer.Tests/ExpressionBuilder/Converters/DefaultConverterTests.cs
@@ -1,5 +1,8 @@
 ﻿using ObjectCartographer.ExpressionBuilder.Converters;
 using ObjectCartographer.Tests.BaseClasses;
+using System.Collections.Generic;
+using System.Text.Json;
+using Xunit;
 
 namespace ObjectCartographer.Tests.ExpressionBuilder.Converters
 {
@@ -8,6 +11,26 @@ namespace ObjectCartographer.Tests.ExpressionBuilder.Converters
         public DefaultConverterTests()
         {
             TestObject = new DefaultConverter();
+        }
+
+        [Fact]
+        public void JsonDocumentToClass()
+        {
+            var ObjectToSerialize = new TestClass { MyProperty = new List<SubTestClass> { new SubTestClass { MyProperty = 5 } } };
+            var SerializedObject = JsonSerializer.Serialize(ObjectToSerialize);
+            var Document = JsonDocument.Parse(SerializedObject);
+            var ResultObject = new TestClass();
+            var Result = TestObject.ConvertTo(Document, ResultObject, typeof(TestClass));
+        }
+
+        private class SubTestClass
+        {
+            public int MyProperty { get; set; }
+        }
+
+        private class TestClass
+        {
+            public List<SubTestClass> MyProperty { get; set; } = new List<SubTestClass>();
         }
     }
 }
